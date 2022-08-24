3225f3630da067f66d36d228805b0d783cd6351c
unknown <olalaadka@gmail.com>
2022-08-24 13:12:55 +0300

    add first calc

---
+++ HW1/script.js
@@ -0,0 +1,6 @@
+const str1 = prompt ("Enter first number");
+const str2 = prompt ("Enter second number");
+const num1 = +str1;
+const num2 = +str2;
+const result = num1 + num2;
+alert (num1+ "+" + num2 + "=" + result);
