901c71ec0d4c6156cb96bb7d08d4e3e8c6b7eefd
unknown <olalaadka@gmail.com>
2022-09-21 15:42:26 +0300

    DOM calc

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const calc = createCalculator(100);
-const calc1 = createCalculator(1000);
+const action = document.querySelector("#pickAction");
+const number1 = document.querySelector("#firstValue");
+const number2 = document.querySelector("#secondValue");
+const result = document.querySelector("#outputResult");
+const calculate = document.querySelector("#calculate");
 
-function createCalculator(val) {
-  return {
-    add: (val2) => {
-      return (val = val + val2);
-    },
-    sub: (val2) => {
-      return (val = val - val2);
-    },
-    div: (val2) => {
-      return (val = val / val2);
-    },
-    mult: (val2) => {
-      return (val = val * val2);
-    },
-    set: (val2) => {
-      return (val = val2);
-    },
-  };
+calculate.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  result.textContent = `${number1.value} ${action.value} 
+  ${number2.value} = ${calc(+number1.value, +number2.value, action.value)}`;
+}
+function calc(a, b, act) {
+  switch (act) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+  }
 }
