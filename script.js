ff48ef60e7ebd39ab39738cf239e125af9b04632
unknown <olalaadka@gmail.com>
2022-10-09 17:16:27 +0300

    create hamburger

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-"use strict";
-
-function Calculator(value) {
-  this.result = value;
-  this.add = function (val) {
-    return (this.result += val);
-  };
-  this.sub = function (val) {
-    return (this.result -= val);
-  };
-
-  this.div = function (val) {
-    return (this.result /= val);
-  };
-  this.mult = function (val) {
-    return (this.result *= val);
-  };
+function YourChoice(price, calories) {
+  (this.price = price), (this.calories = calories);
 }
 
-const calc = new Calculator(10);
+const SMALL_SIZE = new YourChoice(50, 20);
+const MIDDLE_SIZE = new YourChoice(75, 30);
+const Big_SIZE = new YourChoice(100, 40);
+
+const TOPPING_CHEEZE = new YourChoice(10, 20);
+const TOPPING_SALAD = new YourChoice(20, 5);
+const TOPPING_POTATO = new YourChoice(15, 10);
+const TOPPING_SPICE = new YourChoice(15, 0);
+const TOPPING_MAYO = new YourChoice(20, 5);
+
+function Hamburger({ price, calories }) {
+  (this.price = price), (this.calories = calories);
+}
+
+Hamburger.prototype.addtoping = function ({ price, calories }) {
+  return (this.price += price), (this.calories += calories);
+};
+Hamburger.prototype.getPrice = function () {
+  return this.price;
+};
+Hamburger.prototype.getCallories = function () {
+  return this.calories;
+};
+
+const hamburger = new Hamburger(Big_SIZE);
+
+hamburger.addtoping(TOPPING_MAYO);
+hamburger.addtoping(TOPPING_POTATO);
+// hamburger.addtoping(TOPPING_POTATO);
+
+// console.log(hamburger);
+console.log('Price with sauce: ' + hamburger.getPrice());
+console.log('Callories with sauce: ' + hamburger.getCallories());
