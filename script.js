e3a7711b97a7d79495e882a8edad9a448b227770
unknown <olalaadka@gmail.com>
2022-10-20 13:59:26 +0300

    get data

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const accordion = new Accordion(document.querySelector('.container'));
+const tasksList = document.querySelector('#tasks-List');
+const taskTemplate = document.querySelector('#taskTemplate').innerHTML;
+
+let list = [];
+
+init();
+
+function init() {
+  fetchData();
+  renderList(list);
+}
+function fetchData() {
+  fetch('https://jsonplaceholder.typicode.com/todos').then((resp) => {
+    resp.json().then((data) => {
+      list = data;
+      renderList(list);
+    });
+  });
+}
+function renderList(list) {
+  tasksList.innerHTML = list.map(generateListHtml).join('');
+}
+
+function generateListHtml({ id, title, completed }) {
+  return taskTemplate
+    .replaceAll('{{id}}', id)
+    .replaceAll('{{task}}', title)
+    .replaceAll('{{state}}', completed);
+}
